d336333ed1292d22ff1bdfbd828999ea957af95f
ArpitaMishra021 <mishraarpita500@gmail.com>
2026-06-16 12:48:02 +0530

    Added files

---
+++ exp7/src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+function Student(props) {
+  return (
+    <div className="card">
+      <h2>{props.name}</h2>
+      <p>Course: {props.course}</p>
+      <p>Marks: {props.marks}</p>
+    </div>
+  );
+}
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Information</h1>
+      <Student
+        name="Monica Geller"
+        course="BTech (Computer Science)"
+        marks="95"
+      />
+      <Student
+        name="Chandler Bing"
+        course="BTech (Computer Science)"
+        marks="94"
+      />
+      <Student
+        name="Joey Tribbiani"
+        course="BTech (Computer Science)"
+        marks="90"
+      />
+    </div>
+  );
+}
+export default App;

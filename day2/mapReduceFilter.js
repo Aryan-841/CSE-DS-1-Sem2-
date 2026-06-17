20a0c9d7dcf3ec51a794683457947a2e8858f6e9
Aryan <futureboy34345656@gmail.com>
2026-06-17 21:51:59 +0530

    new file

--- day2/mapReduceFilter.js
+++ day2/mapReduceFilter.js
@@ -7,5 +7,7 @@ console.log(multipleOf2);
 const divisibeBy2 = num.filter((n) => n % 2 === 0);
 console.log(divisibeBy2);
 
+
+
 const sumOfNumArray = num.reduce((acc, val) => acc += val, 0);
 console.log(sumOfNumArray);
